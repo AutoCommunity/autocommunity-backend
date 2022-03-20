c530ba3ec99bc2d6762dc24e290a710361528aa4
Maksym Zub <kroistsmeh@gmail.com>
2022-03-20 19:33:03 +0100

    inital commit, spring boot project initialization

---
+++ src/main/java/com/autocommunity/backend/AutocommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autocommunity.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocommunityBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutocommunityBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autocommunity/backend/AutocommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocommunity.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
